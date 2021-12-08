7163e7fb407e08f75c042a9d74ef26c6717419b2
Bjarne Bonde <bb@morningtrain.dk>
2021-12-08 13:35:20 +0100

    PHPDoc upd of context

--- src/Context.php
+++ src/Context.php
@@ -4,6 +4,17 @@ namespace MorningTrain\Laravel\Context;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static meta(string $meta_key, mixed $meta_value) Sets meta value
+ * @method static render(string $blade_view_name) Returns prefixed blade view
+ * @method static view(string $blade_view_name) Returns prefixed blade view
+ * @method static views() Returns prefixed blade view
+ * @method static env(string|\Closure $env_path, mixed|\Closure $value)
+ * @method static routes()
+ * @method static scripts()
+ * @method static stylesheets()
+ * @method static menu()
+ */
 class Context extends Facade
 {
 
